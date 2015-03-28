2eed3299db8f9d0e38d2e1f7b7b0ebe3bfad69ab
Mike <mt_tr1pl@hotmail.com>
2015-03-28 17:33:08 +0100

    dfgdf

---
+++ test.py
@@ -0,0 +1,36 @@
+from random import randint, sample
+from chord import Node
+
+#node_ids = sample(range(Node.ring_size), 2)
+node_ids = [10,11]
+node_failures = 2
+nodes = []
+
+for node_id in node_ids:
+    print('Creating node with node ID: #{}.'.format(node_id))
+    node = Node(node_id)
+    nodes.append(node)
+
+    if len(nodes) <= 1:
+        continue
+
+    known_node = nodes[0]
+        
+    print('Attempting to join network via node ID: #{}.'.format(
+        known_node.node_id))
+
+    node.join(known_node)
+
+    print('Stabilising...')
+    
+    for _ in range(Node.finger_count):
+        for node in nodes:
+            node.stabilise()
+
+        for node in nodes:
+            node.fix_fingers()
+
+    print('Network:')
+    
+    for node in nodes:
+        node.print_fingers()
